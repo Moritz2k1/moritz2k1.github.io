bed19e649d6ea5cea6b03435b5662355ad6c7c3a
Moritz2k1 <moritz.gutschi@outlook.com>
2026-07-19 12:53:51 +0200

    Migrated getters.js

---
+++ src/pokebattle/utils/getters.js
@@ -0,0 +1,20 @@
+import { typeChart } from "../data/typeChart.js";
+import { calcAllStats } from "./calcStats.js";
+
+export function getTypeEffectiveness(moveType, defenderTypes) {
+    let mult = 1;
+
+    for (const defType of defenderTypes) {
+        mult *= typeChart[moveType]?.[defType] ?? 1;
+    }
+
+    return mult;
+}
+
+export function getSpeed(pokemon) {
+    return calcAllStats(pokemon).speed;
+}
+
+export function getMaxHp(pokemon) {
+    return calcAllStats(pokemon).hp;
+}
