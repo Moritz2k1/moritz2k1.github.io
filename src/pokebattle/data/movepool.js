29bdf722dd76f09d0271056d4ebedc0b656f0817
Moritz2k1 <moritz.gutschi@outlook.com>
2026-07-19 13:05:07 +0200

    Fixed movepool.js, migrated wrong file first

--- src/pokebattle/data/movepool.js
+++ src/pokebattle/data/movepool.js
@@ -1,157 +1,390 @@
-export const typeChart = {
-    Normal: {
-        Rock: 0.5,
-        Ghost: 0,
-        Steel: 0.5,
+export const movepool = {
+    vcreate: {
+        name: "V-create",
+        pp: 5,
+        type: "Fire",
+        category: "Physical",
+        power: 180,
+        accuracy: 95,
     },
-    Fire: {
-        Fire: 0.5,
-        Water: 0.5,
-        Grass: 2,
-        Ice: 2,
-        Bug: 2,
-        Rock: 0.5,
-        Dragon: 0.5,
-        Steel: 2,
+
+    boltstrike: {
+        name: "Bolt Strike",
+        pp: 5,
+        type: "Electric",
+        category: "Physical",
+        power: 130,
+        accuracy: 85,
     },
-    Water: {
-        Fire: 2,
-        Water: 0.5,
-        Grass: 0.5,
-        Ground: 2,
-        Rock: 2,
-        Dragon: 0.5,
+
+    psychic: {
+        name: "Psychic",
+        pp: 10,
+        type: "Psychic",
+        category: "Special",
+        power: 90,
+        accuracy: 100,
     },
-    Electric: {
-        Water: 2,
-        Electric: 0.5,
-        Grass: 0.5,
-        Ground: 0,
-        Flying: 2,
-        Dragon: 0.5,
+
+    energyball: {
+        name: "Energy Ball",
+        pp: 10,
+        type: "Grass",
+        category: "Special",
+        power: 90,
+        accuracy: 100,
     },
-    Grass: {
-        Fire: 0.5,
-        Water: 2,
-        Grass: 0.5,
-        Poison: 0.5,
-        Ground: 2,
-        Flying: 0.5,
-        Bug: 0.5,
-        Rock: 2,
-        Dragon: 0.5,
-        Steel: 0.5,
+
+    dragonclaw: {
+        name: "Dragon Claw",
+        pp: 15,
+        type: "Dragon",
+        category: "Physical",
+        power: 80,
+        accuracy: 100,
     },
-    Ice: {
-        Fire: 0.5,
-        Water: 0.5,
-        Grass: 2,
-        Ice: 0.5,
-        Ground: 2,
-        Flying: 2,
-        Dragon: 2,
-        Steel: 0.5,
+
+    earthquake: {
+        name: "Earthquake",
+        pp: 10,
+        type: "Ground",
+        category: "Physical",
+        power: 100,
+        accuracy: 100,
     },
-    Fighting: {
-        Normal: 2,
-        Ice: 2,
-        Poison: 0.5,
-        Flying: 0.5,
-        Psychic: 0.5,
-        Bug: 0.5,
-        Rock: 2,
-        Ghost: 0,
-        Dark: 2,
-        Steel: 2,
-        Fairy: 0.5,
+
+    aerialace: {
+        name: "Aerial Ace",
+        pp: 20,
+        type: "Flying",
+        category: "Physical",
+        power: 60,
+        accuracy: 0,
     },
-    Poison: {
-        Grass: 2,
-        Poison: 0.5,
-        Ground: 0.5,
-        Rock: 0.5,
-        Ghost: 0.5,
-        Steel: 0,
-        Fairy: 2,
+
+    aquatail: {
+        name: "Aqua Tail",
+        pp: 10,
+        type: "Water",
+        category: "Physical",
+        power: 90,
+        accuracy: 90,
     },
-    Ground: {
-        Fire: 2,
-        Electric: 2,
-        Grass: 0.5,
-        Poison: 2,
-        Flying: 0,
-        Bug: 0.5,
-        Rock: 2,
-        Steel: 2,
+
+    hydropump: {
+        name: "Hydro Pump",
+        pp: 5,
+        type: "Water",
+        category: "Special",
+        power: 110,
+        accuracy: 80,
     },
-    Flying: {
-        Electric: 0.5,
-        Grass: 2,
-        Fighting: 2,
-        Bug: 2,
-        Rock: 0.5,
-        Steel: 0.5,
+
+    icebeam: {
+        name: "Ice Beam",
+        pp: 10,
+        type: "Ice",
+        category: "Special",
+        power: 90,
+        accuracy: 100,
     },
-    Psychic: {
-        Fighting: 2,
-        Poison: 2,
-        Psychic: 0.5,
-        Dark: 0,
-        Steel: 0.5,
+
+    dragonpulse: {
+        name: "Dragon Pulse",
+        pp: 10,
+        type: "Dragon",
+        category: "Special",
+        power: 85,
+        accuracy: 100,
     },
-    Bug: {
-        Fire: 0.5,
-        Grass: 2,
-        Fighting: 0.5,
-        Flying: 0.5,
-        Psychic: 2,
-        Ghost: 0.5,
-        Dark: 2,
-        Steel: 0.5,
-        Fairy: 0.5,
+
+    shadowball: {
+        name: "Shadow Ball",
+        pp: 15,
+        type: "Ghost",
+        category: "Special",
+        power: 80,
+        accuracy: 100,
     },
-    Rock: {
-        Fire: 2,
-        Ice: 2,
-        Fighting: 0.5,
-        Ground: 0.5,
-        Flying: 2,
-        Bug: 2,
-        Steel: 0.5,
+
+    darkpulse: {
+        name: "Dark Pulse",
+        pp: 15,
+        type: "Dark",
+        category: "Special",
+        power: 80,
+        accuracy: 100,
     },
-    Ghost: {
-        Normal: 0,
-        Psychic: 2,
-        Ghost: 2,
-        Dark: 0.5,
+
+    sludgebomb: {
+        name: "Sludge Bomb",
+        pp: 10,
+        type: "Poison",
+        category: "Special",
+        power: 90,
+        accuracy: 100,
     },
-    Dragon: {
-        Dragon: 2,
-        Steel: 0.5,
-        Fairy: 0,
+
+    stoneedge: {
+        name: "Stone Edge",
+        pp: 5,
+        type: "Rock",
+        category: "Physical",
+        power: 100,
+        accuracy: 80,
     },
-    Dark: {
-        Fighting: 0.5,
-        Psychic: 2,
-        Ghost: 2,
-        Dark: 0.5,
-        Fairy: 0.5,
+
+    icepunch: {
+        name: "Ice Punch",
+        pp: 15,
+        type: "Ice",
+        category: "Physical",
+        power: 75,
+        accuracy: 100,
     },
-    Steel: {
-        Fire: 0.5,
-        Water: 0.5,
-        Electric: 0.5,
-        Ice: 2,
-        Rock: 2,
-        Steel: 0.5,
-        Fairy: 2,
+
+    megahorn: {
+        name: "Megahorn",
+        pp: 10,
+        type: "Bug",
+        category: "Physical",
+        power: 120,
+        accuracy: 85,
     },
-    Fairy: {
-        Fighting: 2,
-        Dragon: 2,
-        Dark: 2,
-        Fire: 0.5,
-        Poison: 0.5,
-        Steel: 0.5,
+
+    bodyslam: {
+        name: "Bodyslam",
+        pp: 15,
+        type: "Normal",
+        category: "Physical",
+        power: 85,
+        accuracy: 100,
+    },
+
+    thunderpunch: {
+        name: "Thunder Punch",
+        pp: 15,
+        type: "Electric",
+        category: "Physical",
+        power: 75,
+        accuracy: 100,
+    },
+
+    firepunch: {
+        name: "Fire Punch",
+        pp: 15,
+        type: "Fire",
+        category: "Physical",
+        power: 75,
+        accuracy: 100,
+    },
+
+    flamethrower: {
+        name: "Flamethrower",
+        pp: 15,
+        type: "Fire",
+        category: "Special",
+        power: 90,
+        accuracy: 100,
+    },
+
+    airslash: {
+        name: "Air Slash",
+        pp: 15,
+        type: "Flying",
+        category: "Special",
+        power: 75,
+        accuracy: 95,
+    },
+
+    crunch: {
+        name: "Crunch",
+        pp: 15,
+        type: "Dark",
+        category: "Physical",
+        power: 80,
+        accuracy: 100,
+    },
+
+    blizzard: {
+        name: "Blizzard",
+        pp: 5,
+        type: "Ice",
+        category: "Special",
+        power: 110,
+        accuracy: 70,
+    },
+
+    hypervoice: {
+        name: "Hyper Voice",
+        pp: 10,
+        type: "Normal",
+        category: "Special",
+        power: 90,
+        accuracy: 100,
+    },
+
+    brickbreak: {
+        name: "Brick Break",
+        pp: 15,
+        type: "Fighting",
+        category: "Physical",
+        power: 75,
+        accuracy: 100,
+    },
+
+    drainpunch: {
+        name: "Drain Punch",
+        pp: 10,
+        type: "Fighting",
+        category: "Physical",
+        power: 75,
+        accuracy: 100,
+        drain: 0.5,
+    },
+
+    waterfall: {
+        name: "Waterfall",
+        pp: 15,
+        type: "Water",
+        category: "Physical",
+        power: 80,
+        accuracy: 100,
+    },
+
+    rockslide: {
+        name: "Rock Slide",
+        pp: 10,
+        type: "Rock",
+        category: "Physical",
+        power: 75,
+        accuracy: 90,
+    },
+
+    focusblast: {
+        name: "Focus Blast",
+        pp: 5,
+        type: "Fighting",
+        category: "Special",
+        power: 120,
+        accuracy: 70,
+    },
+
+    moonblast: {
+        name: "Moonblast",
+        pp: 15,
+        type: "Fairy",
+        category: "Special",
+        power: 95,
+        accuracy: 100,
+    },
+
+    shadowclaw: {
+        name: "Shadow Claw",
+        pp: 15,
+        type: "Ghost",
+        category: "Physical",
+        power: 70,
+        accuracy: 100,
+    },
+
+    headbutt: {
+        name: "Headbutt",
+        pp: 15,
+        type: "Normal",
+        category: "Physical",
+        power: 70,
+        accuracy: 100,
+    },
+
+    seedbomb: {
+        name: "Seed Bomb",
+        pp: 15,
+        type: "Grass",
+        category: "Physical",
+        power: 80,
+        accuracy: 100,
+    },
+
+    xscissor: {
+        name: "X-Scissor",
+        pp: 15,
+        type: "Bug",
+        category: "Physical",
+        power: 80,
+        accuracy: 100,
+    },
+
+    ironhead: {
+        name: "Iron Head",
+        pp: 15,
+        type: "Steel",
+        category: "Physical",
+        power: 80,
+        accuracy: 100,
+    },
+
+    thunderfang: {
+        name: "Thunder Fang",
+        pp: 15,
+        type: "Electric",
+        category: "Physical",
+        power: 65,
+        accuracy: 95,
+    },
+
+    surf: {
+        name: "Surf",
+        pp: 15,
+        type: "Water",
+        category: "Special",
+        power: 90,
+        accuracy: 100,
+    },
+
+    extrasensory: {
+        name: "Extrasensory",
+        pp: 20,
+        type: "Psychic",
+        category: "Special",
+        power: 80,
+        accuracy: 100,
+    },
+
+    dazzlinggleam: {
+        name: "Dazzling Gleam",
+        pp: 10,
+        type: "Fairy",
+        category: "Special",
+        power: 80,
+        accuracy: 100,
+    },
+
+    drainingkiss: {
+        name: "Draining Kiss",
+        pp: 10,
+        type: "Fairy",
+        category: "Special",
+        power: 50,
+        accuracy: 100,
+        drain: 0.75,
+    },
+
+    alluringvoice: {
+        name: "Alluring Voice",
+        pp: 10,
+        type: "Fairy",
+        category: "Special",
+        power: 80,
+        accuracy: 100,
+    },
+
+    struggle: {
+        name: "Struggle",
+        pp: Infinity,
+        type: "Normal",
+        category: "Physical",
+        power: 50,
+        accuracy: 0,
     },
 };
